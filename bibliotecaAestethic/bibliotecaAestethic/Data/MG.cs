c0d7753112938c3433c433e8e1a86bc6b1dffa95
David-Galeano <98623952+David-Galeano@users.noreply.github.com>
2022-05-28 20:24:12 -0500

    Add files via upload

---
+++ bibliotecaAestethic/bibliotecaAestethic/Data/MG.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
+
+namespace bibliotecaAestethic.Data
+{
+    public class MG
+    {
+        [Key]
+        public int id { get; set; }
+        public string modelo { get; set; }
+        public string nombre { get; set; }
+        public string creador { get; set; }
+        public int porcentajedefuncionamiento { get; set; }
+        public string descripcion { get; set; }
+    }
+}
